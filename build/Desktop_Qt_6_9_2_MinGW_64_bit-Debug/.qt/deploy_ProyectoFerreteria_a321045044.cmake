8c573683c884e7c9c0e114bfc50e5fbbd190fbcf
OscarChilo <ochilo@unsa.edu.pe>
2025-09-19 13:39:03 -0500

    creando el proyecto para un sistema de venta para una fereteria

---
+++ build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/deploy_ProyectoFerreteria_a321045044.cmake
@@ -0,0 +1,8 @@
+include("D:/2025-B/LABORATORIO/LAB-CS/ProyectoFerreteria/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/ProyectoFerreteria-plugins.cmake" OPTIONAL)
+set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
+
+qt6_deploy_runtime_dependencies(
+    EXECUTABLE "D:/2025-B/LABORATORIO/LAB-CS/ProyectoFerreteria/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/ProyectoFerreteria.exe"
+    GENERATE_QT_CONF
+)
