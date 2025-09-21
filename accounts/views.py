6ad0bf21e1fe8fed56cdeaacd9608aa8753b1440
cvenero <cvenero@unsa.edu.pe>
2025-09-21 18:47:31 -0500

    Se agrego funcionalidad para comprobrar usuario existe

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User, auth
 def login_view(request):
-    error = None  # No mostramos nada por defecto
+    error = None # Por defecto, no hay error 
 
     if request.method == 'POST':
-    
-        error = 'Funcionalidad no implementada'
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            print("Login exitoso")
+            return render(request, 'accounts/home.html')
+        else:
+            error = "Credenciales invalidas"
+            print(error)
+            return render(request, 'accounts/login.html', {'error': error})
+    else:
+        return render(request, 'accounts/login.html', {'error': error})
 
-    return render(request, 'accounts/login.html', {'error': error})
+
+def home_view(request):
+    return render(request, 'accounts/home.html')
